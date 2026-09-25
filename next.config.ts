ac3581ea5cfc45e7e62e86341cfca420aa846126
sandschristopher-png <sandschristopher@pm.me>
2026-09-25 14:54:59 -0700

    fix: allow unsplash remote images and update discover profile avatar urls

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
-import type { NextConfig } from "next";
+﻿import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**" },
-      { protocol: "http", hostname: "**" },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
     ],
   },
 };
